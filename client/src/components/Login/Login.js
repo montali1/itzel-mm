783a4e724a667f4488316bba5a3f31bbf28677b3
Itzel-MM <montali1@gmatc.matc.edu>
2020-11-09 15:07:39 -0600

    Completed activity-8-copy

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -35,7 +35,7 @@ const Login = ({ authenticateUser }) => {
       };
 
       const body = JSON.stringify(newUser);
-      const res = await axios.post('/api/login', body, config);
+      const res = await axios.post('http://localhost:5000/api/login', body, config);
 
       // Store user data and redirect
       localStorage.setItem('token', res.data.token);
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
-import Post from './models/Post';
+//import Post from './models/Post';
 import auth from './middleware/auth';
 import path from 'path';
 
